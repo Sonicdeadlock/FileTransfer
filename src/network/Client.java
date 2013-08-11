1887b7cb30a950e286680e68df5fac9e8f69da6e
Sonicdeadlock <sonicdeadlock369@gmail.com>
2013-08-10 17:32:34 -0700

    test commit

--- src/network/Client.java
+++ src/network/Client.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
-	
+	//test
 	public static final int SOCKET=1238;
 	private BufferedReader in;
 	private DataOutputStream out;
