1ab3d37c12970e6a39643b0f9a678635859847fb
sonicdeadlock <sonicdeadlock369@gmail.com>
2013-08-17 00:50:56 -0400

    changes to how data is sent

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,5 +1,7 @@
 package network;
 
+
+
 import java.io.BufferedReader;
 import java.io.DataOutput;
 import java.io.DataOutputStream;
@@ -10,9 +12,9 @@ import java.net.Socket;
 public class Client {
 	
 	public static final int SOCKET=1238;
-	private BufferedReader in;
-	private DataOutputStream out;
-	private Socket socket;
+	private volatile BufferedReader in;
+	private volatile DataOutputStream out;
+	private volatile Socket socket;
 	private CommunicationHandler communicationHandler;
 	
 	public void init(String ip){
@@ -20,10 +22,11 @@ public class Client {
 			socket= new Socket(ip,SOCKET);
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new DataOutputStream(socket.getOutputStream());
+			
 			communicationHandler = new CommunicationHandler(); 
-			communicationHandler.start();
-			if(ip.contains("107"))
-				sendMessage("hi");
+			//communicationHandler.start();
+			sendMessage("hi test");
+			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -32,7 +35,15 @@ public class Client {
 	
 	public void sendMessage(String s){
 		
-		out.writeChars(s);
+		try {
+			socket.getOutputStream().write(s.getBytes());
+			socket.getOutputStream().flush();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
@@ -42,10 +53,12 @@ public class Client {
 		public void run(){
 			while(true){
 				try {
+					if(socket.isBound()){
 					String temp =in.readLine();
 					if(!temp.isEmpty())
 					System.out.println(temp);
-				} catch (IOException e) {
+					}
+				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
