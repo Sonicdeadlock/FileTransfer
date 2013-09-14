d05049e8f72077a06dff463033574e27bf18201b
sonicdeadlock <sonicdeadlock369@gmail.com>
2013-09-13 23:28:16 -0400

    better testing

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -8,8 +8,13 @@ import network.Client;
 public class Test {
 
 	public static void main(String[] args) {
-		Gui g = new Gui(new Logger());
+		Logger logger = new Logger();
+		Gui g = new Gui(logger);
 		g.setVisible(true);
+		Client c =new Client(logger);
+		g.addChat(c);
+		c.init("192.168.1.107");
+		
 	}
 
 }
